1c7dee6f0212a452e879c10126e5fd326ebd4143
AnaLuizaFreschi <freschianaluiza@gmail.com>
2024-12-06 10:35:49 -0300

    Aula 06-12

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import{Header} from "./components/Header"
+import{Footer} from "./components/Footer"
+export function App() {
+  return (
+    <>
+    <Header />
+    <h1>Olá React!</h1>
+    <Footer />
+    </>
+  )
+}
+
